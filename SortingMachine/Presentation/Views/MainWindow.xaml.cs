f0e2bf61f33e399f66c73272679712e496f31b93
Jayla <cjq1632228397@gmail.com>
2026-05-23 01:11:26 +0800

    feat(ui): implement default maximized window state and custom window chrome buttons

--- SortingMachine/Presentation/Views/MainWindow.xaml.cs
+++ SortingMachine/Presentation/Views/MainWindow.xaml.cs
@@ -1,8 +1,34 @@
 using System.Windows;
+using System.Windows.Input;
 
 namespace SortingMachine.Presentation.Views;
 
 public partial class MainWindow : Window
 {
     public MainWindow() => InitializeComponent();
+
+    private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+    {
+        e.CanExecute = true;
+    }
+
+    private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+    {
+        SystemCommands.MinimizeWindow(this);
+    }
+
+    private void CommandBinding_Executed_Maximize(object sender, ExecutedRoutedEventArgs e)
+    {
+        SystemCommands.MaximizeWindow(this);
+    }
+
+    private void CommandBinding_Executed_Restore(object sender, ExecutedRoutedEventArgs e)
+    {
+        SystemCommands.RestoreWindow(this);
+    }
+
+    private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+    {
+        SystemCommands.CloseWindow(this);
+    }
 }
